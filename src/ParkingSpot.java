cefbd7645265c5c7d6e427b6d1f434e43b6ce75a
spongebobkzkz <130143596+spongebobkzkz@users.noreply.github.com>
2026-02-04 10:36:16 +0500

    Update ParkingSpot.java

--- src/ParkingSpot.java
+++ src/ParkingSpot.java
@@ -2,17 +2,16 @@ package entities;
 
 public class ParkingSpot {
     private int id;
-    private String type; // "Regular" немесе "Electric"
-    private boolean available; // бұрын isFree еді
+    private String type;
+    private boolean available;
 
-    // Толық конструктор
+   
     public ParkingSpot(int id, String type, boolean available) {
         this.id = id;
         this.type = type;
         this.available = available;
     }
 
-    // Қысқа конструктор қажет болса
     public ParkingSpot(int id, boolean available) {
         this.id = id;
         this.type = "Regular"; // default
